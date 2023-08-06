1ad24f6af3b46dfc8b986bc3931cbd8da4d05e16
PromiSe#### <vicimpa@ya.ru>
2023-08-07 06:36:50 +0700

    Update socket-api

--- socket-api/src/index.ts
+++ socket-api/src/index.ts
@@ -1,6 +1,10 @@
 export type TApi = { [key: string]: (...args: any[]) => any; };
+export type TPromiseApi<T extends TApi> = {
+  [key in keyof T]: (...args: Parameters<T[key]>) => Promise<ReturnType<T[key]>>
+};
 export type TSocket = {
   on(name: string, callback: (...args: any[]) => any): any;
+  off(name: string, callback: (...args: any[]) => any): any;
   emit(name: string, ...args: any[]): any;
 };
 
@@ -11,17 +15,41 @@ const useApi = <T extends TApi>(socket: TSocket): T => {
         throw new Error('Method not with symbol');
 
       return (...args: any) => {
-        socket.emit(key, ...args);
+        return new Promise((resolve, reject) => {
+          socket.emit(key, args, (err: Error | null, result: any) => {
+            if (err) return reject(err);
+            resolve(result);
+          });
+        });
       };
     }
   });
 };
 
 const forwardApi = <T extends TApi>(socket: TSocket, api: Partial<T>) => {
+  const unforwardSet = new Set<Function>();
+
   for (const key in api) {
-    if (api[key] instanceof Function)
-      socket.on(key, (api as any)[key]);
+    if (api[key] instanceof Function) {
+      const func = (args: any[], callback: (err: Error | null, result: any) => any) => {
+        Promise.resolve()
+          .then(() => (api as any)[key](...args))
+          .then(result => callback(null, result))
+          .catch(err => callback(err, null));
+      };
+
+      socket.on(key, func);
+      unforwardSet.add(() => socket.off(key, func));
+    }
   }
+
+  return () => {
+    for (const unforward of unforwardSet) {
+      unforward();
+    }
+
+    unforwardSet.clear();
+  };
 };
 
 export { forwardApi, useApi };
