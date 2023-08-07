84751e275ba40eb4e7ddd5e9b0f0631739e0c943
PromiSe#### <vicimpa@ya.ru>
2023-08-07 23:13:36 +0700

    Fix short

--- client/src/components/Short.svelte
+++ client/src/components/Short.svelte
@@ -1,18 +1,40 @@
-<span>
-  <slot />
-</span>
+<script lang="ts">
+  import { onFrame } from "library/onFrame";
+
+  let container: HTMLDivElement;
+  let element: HTMLSpanElement;
+  let left = 0;
+
+  onFrame(() => {
+    const { offsetWidth: WIDTH } = container;
+    const { offsetWidth: width } = element;
+
+    left = WIDTH / 2 - width / 2;
+  });
+</script>
+
+<div bind:this={container} class="short">
+  <span bind:this={element} style="left: {left}px">
+    <slot />
+  </span>
+</div>
 
 <style lang="sass">
-  span
+  
+  .short
     position: absolute
     margin: auto
     left: 0
     right: 0
     bottom: calc(100% + 2px)
-    background-color: #000
-    padding: 2px
     font-size: 4px
-    opacity: 0.3
     text-align: center
     z-index: 1
+
+    span
+      position: absolute
+      padding: 2px
+      background-color: #000
+      opacity: .3
+      bottom: 0
 </style>
