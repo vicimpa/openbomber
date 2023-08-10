194e6ca426dd9276c163825746d0a10b1c1945d0
PromiSe#### <vicimpa@ya.ru>
2023-08-10 20:38:29 +0700

    Append Buffers and Proto

---
+++ src/library/Buffers.ts
@@ -0,0 +1,64 @@
+const types = {
+  int8: new Int8Array(1),
+  uint8: new Uint8Array(1),
+  float32: new Float32Array(1),
+  float64: new Float64Array(1)
+};
+
+export type TTypes = typeof types;
+export type TType = keyof TTypes;
+export type TGType<T extends TType> = TTypes[T] & {
+  constructor: typeof Float32Array;
+};
+
+export class WriteBuffer {
+  #buffers: ArrayBuffer[] = [];
+
+  get size() {
+    return this.#buffers
+      .reduce((acc, { byteLength: l }) => acc + l, 0);
+  }
+
+  concat(): ArrayBuffer {
+    const buffers = this.#buffers;
+    const tmp = new Uint8Array(this.size);
+
+    for (let i = 0, c = 0; i < buffers.length; i++) {
+      tmp.set(new Uint8Array(buffers[i]), c);
+      c += buffers[i].byteLength;
+    }
+
+    return tmp.buffer;
+  }
+
+  write<T extends TType>(type: T, value: number): void {
+    const tmp = types[type];
+    tmp[0] = value;
+    this.#buffers.push(tmp.buffer.slice(0));
+  }
+
+  flush(): ArrayBuffer {
+    const buffer = this.concat();
+    this.#buffers.splice(0);
+    return buffer;
+  }
+}
+
+export class ReadBuffer {
+  #buffer = new ArrayBuffer(0);
+  #cursor = 0;
+
+  accept(buffer: ArrayBuffer) {
+    this.#cursor = 0;
+    this.#buffer = buffer;
+  }
+
+  read<T extends TType>(type: T) {
+    const valueType = types[type];
+    const { buffer, constructor: Type } = valueType as TGType<T>;
+    const { byteLength: size } = buffer;
+    const oldCursor = this.#cursor;
+    this.#cursor += size;
+    return new Type(this.#buffer.slice(oldCursor, oldCursor + size))[0];
+  }
+}
---
+++ src/proto.ts
@@ -0,0 +1,46 @@
+import { ReadBuffer, WriteBuffer } from "./library/Buffers";
+
+import type { Player } from "../server/class/Player";
+
+export class PlayerPositionsProto {
+  static readable = new ReadBuffer();
+  static writable = new WriteBuffer();
+
+  static from(buffer: ArrayBuffer) {
+    const { readable } = this;
+
+    readable.accept(buffer);
+
+    const output: Player['posInfo'][] = [];
+    const size = readable.read('uint8');
+
+    for (let i = 0; i < size; i++) {
+      output.push({
+        id: readable.read('uint8'),
+        x: readable.read('float32'),
+        y: readable.read('float32'),
+        dir: readable.read('uint8'),
+        animate: readable.read('uint8'),
+      });
+    }
+
+    return output;
+  }
+
+  static to(players: Player['posInfo'][]) {
+    const { writable } = this;
+
+    writable.write('uint8', players.length);
+
+    for (let i = 0; i < players.length; i++) {
+      const player = players[i];
+      writable.write('uint8', player.id);
+      writable.write('float32', player.x);
+      writable.write('float32', player.y);
+      writable.write('uint8', player.dir);
+      writable.write('uint8', player.animate);
+    }
+
+    return writable.flush();
+  }
+}
