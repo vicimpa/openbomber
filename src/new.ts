f1c3532121e911490f33ecb48808efec5828d3e2
PromiSe#### <vicimpa@ya.ru>
2023-08-15 20:58:48 +0700

    Return new engine

--- src/new.ts
+++ src/new.ts
@@ -1,18 +1,5 @@
-import { PlayerSetPositionProto } from "@/proto";
+import App from "./new.svelte";
 
-// import App from "./new.svelte";
-
-// new App({
-//   target: document.getElementById('app')!
-// });
-
-const buffer = PlayerSetPositionProto.from({
-  x: 1.2,
-  y: 2.3,
-  dir: 4,
-  animate: 5
+new App({
+  target: document.getElementById('app')!
 });
-
-const data = PlayerSetPositionProto.to(buffer);
-
-console.log(data);
