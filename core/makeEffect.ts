0403c0318ecc6ded8afa27c63237a3405b9f42bb
PromiSe#### <vicimpa@ya.ru>
2023-08-16 16:37:47 +0700

    Fix error

--- core/makeEffect.ts
+++ core/makeEffect.ts
@@ -6,12 +6,19 @@ export const makeEffect = <T>() => {
   let previewValue: T | null = null;
 
   return (newValue: T, callback: (value: T, previewValue: T | null) => any) => {
-    if (newValue instanceof ArrayBuffer && previewValue instanceof ArrayBuffer) {
-      if (isEqualBuffer(previewValue, newValue))
-        return;
-    } else {
-      if (equal(previewValue, newValue))
-        return;
+    try {
+      if (newValue instanceof ArrayBuffer && previewValue instanceof ArrayBuffer) {
+        if (isEqualBuffer(previewValue, newValue))
+          return;
+      } else {
+        if (equal(previewValue, newValue))
+          return;
+      }
+    } catch (e) {
+
+      callback(newValue, previewValue);
+      previewValue = newValue;
+      return;
     }
 
     callback(newValue, previewValue);
