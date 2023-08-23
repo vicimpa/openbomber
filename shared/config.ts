dd304136d7d06ce9d26b6dbd9bb7d8d53466adf4
PromiSe#### <vicimpa@ya.ru>
2023-08-23 19:41:15 +0700

    Update times

--- shared/config.ts
+++ shared/config.ts
@@ -1,13 +1,15 @@
+import { FDate } from "../core/FDate";
+
 export const NICK_LENGTH = 18;
 export const MESSAGE_LENGTH = 100;
-export const PLAYER_TIMEOUT = 60000;
-export const SHIELD_TIME = 30000;
-export const SPEED_TIME = 20000;
-export const CRAZY_BOMB_TIME = 30000;
-export const BOMB_TIME = 2000;
-export const CRAZY_BOMB_MIN = 500;
-export const CRAZY_BOMB_MAX = 5000;
+export const PLAYER_TIMEOUT = FDate.from('1m');
+export const SHIELD_TIME = FDate.from('50s');
+export const SPEED_TIME = FDate.from('25s');
+export const CRAZY_BOMB_TIME = FDate.from('30s');
+export const BOMB_TIME = FDate.from('2s');
+export const CRAZY_BOMB_MIN = FDate.from('500');
+export const CRAZY_BOMB_MAX = FDate.from('5s');
 export const CRAZY_BOMB_BOOST = 5;
 export const SKINS_COUNT = 52;
-export const TIMEOUT_MESSAGE = 5000;
-export const TIMEOUT_RECONNECT = 10000;
+export const TIMEOUT_MESSAGE = FDate.from('5s');
+export const TIMEOUT_RECONNECT = FDate.from('10s');
