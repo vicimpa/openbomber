71513f345f974d5b654bc2feebb92f93ef1f565b
PromiSe#### <vicimpa@ya.ru>
2023-08-24 16:26:45 +0700

    Create PWA

---
+++ public/service.js
@@ -0,0 +1,41 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches
+      .open("v1-data")
+      .then(function (cache) {
+        const urlsToCache = [
+          location.href, 
+          "index.html", 
+          "manifest.json", 
+          "favicon.png",
+          "assets",
+          "images"
+        ];
+        cache.addAll(urlsToCache);
+      })
+      .catch(console.error)
+  );
+});
+
+self.addEventListener("activate", event => {
+  var cacheKeeplist = ["v1-data"];
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).catch(console.error)
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    }).catch((e) => { })
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { createDOM } from "library/utils";
 import App from "./app.svelte";
 
 new App({
@@ -7,3 +8,25 @@ new App({
 addEventListener('keydown', (e) => {
   if (e.code === 'Tab') e.preventDefault();
 });
+
+if (location.protocol == 'https:') {
+  const appendTo = document.head;
+
+  createDOM('link', { rel: 'manifest', href: './manifest.json', appendTo });
+  createDOM('link', { rel: 'shortcut icon', href: 'favicon.png', type: 'image/png', appendTo });
+
+  addEventListener("load", () => {
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.getRegistration('./service.js')
+        .then(e => e ? e : navigator.serviceWorker
+          .register("./servise.js", {
+            scope: location.pathname
+          }))
+        .catch(() => { });
+    }
+
+    window.matchMedia(
+      '(display-mode: standalone)'
+    ).matches;
+  });
+}
