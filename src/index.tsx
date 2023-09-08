a99f547009c7bfe7cb629449d1b872354e723ecd
PromiSe#### <vicimpa@ya.ru>
2023-09-08 22:11:33 +0700

    Make index component

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
+import "library/gamepads";
+
 import { AppReact } from "app";
 import { createRoot } from "react-dom/client";
 
 import AppSvelte from "./app.svelte";
 
-if (location.hash !== '#new') {
-  new AppSvelte({
-    target: document.getElementById('root')!
-  });
-} else {
-  createRoot(document.getElementById('root')!)
-    .render(<AppReact />);
-}
+new AppSvelte({
+  target: document.getElementById('root')!
+});
+
+// createRoot(document.getElementById('root')!)
+//   .render(<AppReact />);
 
 addEventListener('keydown', (e) => {
   if (e.code === 'Tab') e.preventDefault();
