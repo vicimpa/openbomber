cdb7dda3115a86e3890baa170b09d9a484fac2ae
PromiSe#### <vicimpa@ya.ru>
2023-09-08 22:10:41 +0700

    Complate panel component

--- src/components/react/Panel/Panel.tsx
+++ src/components/react/Panel/Panel.tsx
@@ -1,16 +1,16 @@
-import type { FC, ReactNode } from "react";
-
 import styles from "./Panel.module.sass";
 
+import type { FC } from "react";
 export type TPanelProps = {
-  children?: ReactNode;
-};
+  padding?: string;
+} & Omit<JSX.IntrinsicElements['div'], 'ref'>;
 
-export const Panel: FC<TPanelProps> = ({ children }) => {
-
+export const Panel: FC<TPanelProps> = ({ children, padding, className, ...props }) => {
   return (
-    <div className={styles.panel}>
-      {children}
+    <div className={`${styles.panel} ${className}`} {...props}>
+      <div style={{ padding }} className={styles.content}>
+        {children}
+      </div>
     </div>
   );
 };
